b5f173b7d7cff38da2aebb8aa43ae90da1b97ee6
Edvaldo Leandro Gomes da Silva <Edvaldo Leandro Gomes da Silva>
2017-03-18 18:37:43 +0100

    Primeiro código

---
+++ Arm/Arm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arm
+{
+    class Program
+    {
+         public static bool narcisista(int narc)
+        {
+            int r=narc;
+            int[] v =new int [10];
+            int i = 0;
+             while (r != 0)
+             {
+                 v[i++] = r % 10;
+                 r = (r - (r % 10)) / 10;
+             }
+             for (int a = 0; a < i; a++)
+             {
+                 r += (int)Math.Pow(v[a], i);
+             }
+             return narc == r;
+                
+        }
+        static void Main(string[] args)
+        {
+
+             Console.WriteLine(narcisista(153));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ _Teste/_Teste/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Testefactorial
+{
+    class Program
+    {
+        public bool narcisista(int narc)
+        {
+            Console.WriteLine(153 / 10);
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(narcisista(153));
+            Console.ReadKey();
+        }
+    }
+}
